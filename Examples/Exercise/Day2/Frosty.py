c88aeb3206ac9342325033827264cab2583dfd5a
Alexis-pr <53275027+Alexis-pr@users.noreply.github.com>
2025-11-05 18:59:27 -0500

    Add files via upload

---
+++ Examples/Exercise/Day2/Frosty.py
@@ -0,0 +1,11 @@
+
+chocolate = 4000
+vainilla = 3500
+
+print(input("Bienvenido a heladeria Frosty, sabores disponibles ingrese un sabor: "))
+
+print(input("Desea topping?: "))
+topping = 1000
+
+
+
