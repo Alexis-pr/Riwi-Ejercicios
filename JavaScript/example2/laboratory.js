a5568910191c767dc27c40edcc827c5dbeb7cb1b
Maryhug viviana duran pedraza <maryhug_duran82231@elpoli.edu.co>
2026-01-06 17:52:43 -0500

    estilos para los logs

--- JavaScript/example2/laboratory.js
+++ JavaScript/example2/laboratory.js
@@ -8,17 +8,17 @@ let estado = prompt("¿Estás activo? (true/false)").toLowerCase();
 //estado = Boolean(estado) // Convertir edad a booleano
 estado = estado === "true";
 //ingresoDatos();    
-
+console.group("Tipos de Datos del usuario:");
 console.log(typeof nombre + " " + nombre); // string
 console.log(typeof edad + " " + edad);   // number  
 console.log(typeof correo + " " + correo) ; // string
 console.log(typeof estado + " " +  estado); // boolean
-
+console.groupEnd();
 
 
 
 // Punto 2
-
+console.group("Validación de Edad:");
 validarEdad(edad);
 
 //&& estado != false || estado != 0 || estado != null || estado != undefined || estado != NaN
@@ -28,13 +28,17 @@ if (nombre != "" && estado == true) {
      console.log("Usuario con estado inactivo o sin nombre registrado.");
 }
 
+console.groupEnd();
+
 // Punto 3
 
 contraseñaIngresada= prompt("Por favor, ingresa una contraseña (mínimo 8 caracteres):");
 intento = 0;
 
+console.group("Validación de Contraseña:");
 validarContraseña(contraseñaIngresada , intento);
 
+console.groupEnd();
 
 // Punto 4 -- funciones
 
@@ -65,10 +69,13 @@ validarContraseña(contraseñaIngresada , intento);
 
 
 function validarEdad(edad) {
-    if (edad >= 18) {
-       return console.log(`${nombre} es mayor de edad.`);
-    }else {
-        return console.log(`${nombre} es menor de edad.`);
+
+    if (edad < 0 || isNaN(edad)) {
+        console.error("Edad inválida. Por favor, ingresa una edad válida.");
+    } else if (edad >= 18) {
+        console.log("Es mayor de edad.");
+    } else {
+        console.warn("Es menor de edad.");
     }
 }
 
@@ -77,21 +84,21 @@ function validarContraseña(contraseña , intento) {
     while (intento < 2 ) {
    
         if (contraseña === contraseñaGuardada) {
-            console.log("Contraseña correcta. Acceso concedido.");  
+            // %c para dar estilo al mensaje en la consola
+            console.log("%c Contraseña Correcta. Acceso concedido", "color: white ; background-color: green; font-size: 15px ; font: small-caps 100%/200% serif;");
+            
             break;
         }else {
-            console.log("Contraseña incorrecta. Intenta de nuevo.");
-            contraseñaIngresada = prompt("Por favor, ingresa una contraseña (mínimo 8 caracteres):");
+            console.log("%c Contraseña incorrecta. Intenta de nuevo.", "color: white ; background-color: red ; font-size: 15px ; font: small-caps 100%/200% serif;");
             
+            contraseñaIngresada = prompt("Por favor, ingresa una contraseña (mínimo 8 caracteres):");
+            contraseña = contraseñaIngresada; // actualiza la contraseña ingresa nuevamente para entrar en el siguiente ciclo
         }
-    
         intento++;
         if (intento == 2) {
-            console.log("Has agotado todos los intentos. Acceso denegado.");   
+            console.log("%c Has agotado todos los intentos. Acceso denegado.", "color: white ; background-color: red;font-size: 15px ; font: small-caps 100%/200% serif;");  
             break;         
         }
-
-    }   
-   
+    }      
 }
 
