b2f69b0138bb4d5524752602f35a9bff22756961
Alexis-pr <Yonathan.yp20@gmail.com>
2026-01-13 20:28:43 -0500

    texto cambiado

--- JavaScript/example3/app.js
+++ JavaScript/example3/app.js
@@ -5,3 +5,9 @@ const titulo = document.getElementById('title');
 btnCambiarColor.addEventListener('click', () => {
     titulo.style.color = titulo.style.color === 'blue' ? 'black' : 'blue';
 });
+
+const btnCambiarTexto = document.getElementById('changeTextBtn');
+
+btnCambiarTexto.addEventListener('click', () => {
+    titulo.textContent = titulo.textContent === 'Hello, World!' ? 'Text Changed!!!!' : 'Hello, World!';
+}); 
